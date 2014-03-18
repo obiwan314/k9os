b6578f7c4106b81dc7f351505e0673b138ef72d4
William Reichardt <william.reichardt@gmail.com>
2014-03-17 23:05:09 -0400

    Add bin directory

---
+++ bin/install.sh
@@ -0,0 +1,12 @@
+# OSX ARD_SKETCH=/Users/wreichardt/Documents/Arduino
+# OSX $ARD_LIB=/Users/wreichardt/Documents/Arduino/libraries
+# PI ARD_SKETCH=/home/pi/sketchbook/
+# PI $ARD_LIB=/usr/share/arduino/libraries
+export ARD_SKETCH=/home/pi/sketchbook/
+export ARD_LIB=/home/pi/sketchbook/libraries
+mkdir $ARD_LIB
+cd ..
+ln -s `pwd`/arduino/panel/libraries/LiquidCrystal $ARD_LIB/LiquidCrystal
+ln -s `pwd`/arduino/panel/libraries/aJson $ARD_LIB/aJason
+ln -s `pwd`/arduino/panel/src/k9os $ARD_SKETCH
+ln -s `pwd`/arduino/motion/src/drive $ARD_SKETCH
