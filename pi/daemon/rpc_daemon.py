eb168255d5d7915b66d9c4d10585f9db4c6447d1
William Reichardt <william.reichardt@gmail.com>
2014-04-15 00:47:31 -0400

    Basic web controls work now.

--- pi/daemon/rpc_daemon.py
+++ pi/daemon/rpc_daemon.py
@@ -20,6 +20,60 @@ class K9Service(rpyc.Service):
         panel.rotate(panel.DIRECTION_RIGHT)
     def exposed_stop(self, *args, **kws):
         panel.rotate(panel.DIRECTION_STOP)
+    def exposed_go_forward(self, *args, **kws):
+        panel.go_forward(args[0])
+    def exposed_go_backward(self, *args, **kws):
+        panel.go_backward(args[0])
+    def exposed_clear_lcd(self, *args, **kws):
+        panel.clear_lcd()
+    def exposed_lcd_back_light_on(self, *args, **kws):
+        panel.lcd_back_light_on()
+    def exposed_lcd_back_light_off(self, *args, **kws):
+        panel.lcd_back_light_off()
+    def exposed_test_mode_on(self, *args, **kws):
+        panel.test_mode_on()
+    def exposed_test_mode_off(self, *args, **kws):
+        panel.test_mode_off()
+    def exposed_lcd_write(self, *args, **kws):
+        panel.lcd_write(args[0],args[1])
+    def exposed_lcd_write_bulk(self, *args, **kws):
+        panel.lcd_write_bulk(args[0],args[1],args[2])
+    def exposed_lcd_write_line1(self, *args, **kws):
+        panel.lcd_write_line1(args[0])
+    def exposed_lcd_write_line2(self, *args, **kws):
+        panel.lcd_write_line2(args[0])
+    def exposed_lcd_write_line3(self, *args, **kws):
+        panel.lcd_write_line3(args[0])
+    def exposed_lcd_write_line4(self, *args, **kws):
+        panel.lcd_write_line4(args[0])
+    def exposed_cancel_animation(self, *args, **kws):
+        panel.cancel_animation()
+    def exposed_set_light_code(self, *args, **kws):
+        panel.set_light_code(args[0])
+    def exposed_animation_vertical_sweep_right(self, *args, **kws):
+        panel.animation_vertical_sweep_right()
+    def exposed_animation_vertical_sweep_left(self, *args, **kws):
+        panel.animation_vertical_sweep_left()
+    def exposed_animation_horizontal_sweep_down(self, *args, **kws):
+        panel.animation_horizontal_sweep_down()
+    def exposed_animation_diagonal_lr_top(self, *args, **kws):
+        panel.animation_diagonal_lr_top()
+    def exposed_animation_diagonal_rl_top(self, *args, **kws):
+        panel.animation_diagonal_rl_top()
+    def exposed_animation_strobe(self, *args, **kws):
+        panel.animation_strobe()
+    def exposed_animation_blinking_right_arrow(self, *args, **kws):
+        panel.animation_blinking_right_arrow()
+    def exposed_animation_blinking_left_arrow(self, *args, **kws):
+        panel.animation_blinking_left_arrow()
+    def exposed_animation_diagonal_lr_bottom(self, *args, **kws):
+        panel.animation_diagonal_lr_bottom()
+    def exposed_animation_diagonal_rl_bottom(self, *args, **kws):
+        panel.animation_diagonal_rl_bottom()
+    def exposed_animation_horizontal_sweep_up(self, *args, **kws):
+        panel.animation_horizontal_sweep_up()
+    def exposed_animation_random(self, *args, **kws):
+        panel.animation_random()
 
 if __name__ == '__main__':
     s=rpyc.utils.server.ThreadedServer(K9Service, port=12345)
--- pi/web/k9webserver.py
+++ pi/web/k9webserver.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import datetime
+import rpyc
 app = Flask(__name__)
+conn = rpyc.connect('192.168.10.10',12345)
 
 @app.route("/")
 def hello():
@@ -11,6 +13,61 @@ def hello():
       'time': timeString
       }
    return render_template('main.html', **templateData)
+
+@app.route("/rotate_left")
+def rotate_left():
+    conn.root.rotate_left()
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+        'title' : 'HELLO!',
+        'time': timeString
+    }
+    return render_template('main.html', **templateData)
+
+@app.route("/rotate_right")
+def rotate_right():
+    conn.root.exposed_rotate_right()
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+        'title' : 'HELLO!',
+        'time': timeString
+    }
+    return render_template('main.html', **templateData)
+
+@app.route("/stop_all")
+def stop_all():
+    conn.root.stop()
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+        'title' : 'HELLO!',
+        'time': timeString
+    }
+    return render_template('main.html', **templateData)
+
+@app.route("/go_forward")
+def go_forward():
+    conn.root.go_forward(30)
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+        'title' : 'HELLO!',
+        'time': timeString
+    }
+    return render_template('main.html', **templateData)
+
+@app.route("/go_backward")
+def go_backward():
+    conn.root.go_backward(30)
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+        'title' : 'HELLO!',
+        'time': timeString
+    }
+    return render_template('main.html', **templateData)
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
